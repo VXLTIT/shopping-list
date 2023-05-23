cc03badab3d18c52acf508cd2f71f7df58efbe24
VXLTIT <danil050706@gmail.com>
2023-05-23 17:37:30 +0500

    added the ability to create lists (incomplete)

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
+let allLists = document.querySelector('#lists');
+let countLists = 1;
+let inputName = document.querySelector('#inputName')
+let createListButton = document.querySelector('#createList');
 
+createListButton.addEventListener('click', createList);
+
+
+function createList() {
+    let name = document.createElement('h3');
+    name.textContent = inputName.value;
+
+    let inputItemName = document.createElement('input');
+    let buttonCreateItem = document.createElement('button');
+    let list = document.createElement('div');
+    let orderedItemList = document.createElement('ol');
+
+    buttonCreateItem.textContent = 'Добавить';
+    
+    list.id = 'list' + countLists;
+    list.className = 'list';
+
+    allLists.appendChild(list);
+    
+    list.appendChild(name);
+    list.appendChild(inputItemName);
+    list.appendChild(buttonCreateItem);
+    list.appendChild(orderedItemList);
+
+    countLists++;
+}
