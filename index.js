14784f633041086381f853751507fd39a38b332a
VXLTIT <danil050706@gmail.com>
2023-05-24 20:43:33 +0500

    -Аdded the ability to add a list item. -Development of the ability to delete lists has begun

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let allLists = document.querySelector('#lists');
 let countLists = 1;
-let inputName = document.querySelector('#inputName')
+let inputName = document.querySelector('#inputName');
 let createListButton = document.querySelector('#createList');
 
 createListButton.addEventListener('click', createList);
@@ -10,23 +10,51 @@ createListButton.addEventListener('click', createList);
 function createList() {
     let name = document.createElement('h3');
     name.textContent = inputName.value;
+    inputName.value = '';
 
     let inputItemName = document.createElement('input');
     let buttonCreateItem = document.createElement('button');
     let list = document.createElement('div');
     let orderedItemList = document.createElement('ol');
+    let removeListButton = document.createElement('button');
+    let listNameBlock = document.createElement('div');
+
+    listNameBlock.id = 'listNameBlock';
+    removeListButton.id = 'removeListButton';
 
     buttonCreateItem.textContent = 'Добавить';
+    removeListButton.textContent = 'Удалить лист';
     
     list.id = 'list' + countLists;
     list.className = 'list';
 
     allLists.appendChild(list);
-    
-    list.appendChild(name);
+    list.appendChild(listNameBlock);
+
+    listNameBlock.appendChild(name);
+    listNameBlock.appendChild(removeListButton);
+
     list.appendChild(inputItemName);
     list.appendChild(buttonCreateItem);
     list.appendChild(orderedItemList);
 
+    buttonCreateItem.addEventListener('click', createItem);
+    removeListButton.addEventListener('click', removeList);
     countLists++;
+
+    function createItem() {
+
+    let item = document.createElement('li');
+    item.textContent = inputItemName.value;
+    inputItemName.value = '';
+
+    orderedItemList.appendChild(item);
+
+    }
+
+    function removeList() {
+        list.remove();
+    }
+
 }
+
